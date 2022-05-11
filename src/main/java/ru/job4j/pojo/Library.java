db43c7c85b2fc7a6e696a6855c9b3d5cd2012696
Arthur Magomedov <devmagomedov@gmail.com>
2022-05-11 16:33:51 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,32 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book bookOne = new Book("Clean code", 400);
+        Book bookTwo = new Book("Pushkin", 300);
+        Book bookThree = new Book("Dostaevskiy", 122);
+        Book bookFour = new Book("Tolstoy", 700);
+        Book[] books = new Book[4];
+        books[0] = bookOne;
+        books[1] = bookTwo;
+        books[2] = bookThree;
+        books[3] = bookFour;
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            System.out.println(bk.getBookName() + " - " + bk.getNumberOfPages() + " pages");
+        }
+        Book booksReserve = books[0];
+        books[0] = books[3];
+        books[3] = booksReserve;
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            System.out.println(bk.getBookName() + " - " + bk.getNumberOfPages() + " pages");
+        }
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            if (bk.getBookName().equals("Clean code")) {
+                System.out.println(bk.getBookName() + " - " + bk.getNumberOfPages() + " pages");
+            }
+        }
+    }
+}

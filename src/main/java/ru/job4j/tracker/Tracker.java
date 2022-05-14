0507dc6320d8e17687a31f5449be95196ef59e2d
Arthur Magomedov <devmagomedov@gmail.com>
2022-05-14 12:13:39 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -62,6 +62,6 @@ public class Tracker {
         System.arraycopy(items, index + 1, items, index, size - index - 1);
         items[size - 1] = null;
         size--;
-        return indexOf(id) != -1;
+        return index != -1;
     }
 }

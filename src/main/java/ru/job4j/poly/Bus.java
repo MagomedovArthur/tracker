a281872b3e6b96f9ca91742cb4c51a940bc778c4
Arthur Magomedov <devmagomedov@gmail.com>
2022-05-19 22:06:14 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int count) {
+
+    }
+
+    @Override
+    public double refuel(double liters) {
+        return 0;
+    }
+}

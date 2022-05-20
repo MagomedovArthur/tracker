67a1f2b3c7a16bdefa97cbb7e3d5a815b698339b
Arthur Magomedov <devmagomedov@gmail.com>
2022-05-20 10:52:51 +0300

    3. Интерфейс Input [#181092 #302874]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,16 +4,16 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
-
+        System.out.println("The bus is going...");
     }
 
     @Override
     public void passengers(int count) {
-
+        System.out.println("The bus has " + count + " passengers.");
     }
 
     @Override
     public double refuel(double liters) {
-        return 0;
+        return 46.70 * liters;
     }
 }

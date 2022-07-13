a2125ae5feb0706c7c53e4f035b90a5b9eef4f47
Arthur Magomedov <devmagomedov@gmail.com>
2022-07-13 12:14:41 +0300

    2. Очередь с приоритетом на LinkedList [#41670 #321315]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -15,7 +15,7 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() < task.getPriority()) {
+            if (task.getPriority() < element.getPriority()) {
                 break;
             }
             index++;

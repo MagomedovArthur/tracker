ef80c1204ffd0d322d1fc5b1f1e685c40cee3f98
Arthur Magomedov <devmagomedov@gmail.com>
2022-08-27 15:59:41 +0300

    2. Комбинированный компаратор [#10036 #336564]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -9,48 +9,56 @@ import static org.junit.Assert.assertThat;
 
 class JobTest {
 
-    Job job1 = new Job("Fix bug", 0);
-    Job job2 = new Job("Fix bug", 4);
-    Comparator<Job> comparator;
-
     @Test
     void whenAscByName() {
-        comparator = new JobAscByName();
+        Job job1 = new Job("Fix bug", 0);
+        Job job2 = new Job("Fix bug", 4);
+        Comparator<Job> comparator = new JobAscByName();
         int rsl = comparator.compare(job1, job2);
         assertThat(rsl, is(0));
     }
 
     @Test
     void whenAscByPriority() {
-        comparator = new JobAscByPriority();
+        Job job1 = new Job("Fix bug", 0);
+        Job job2 = new Job("Fix bug", 4);
+        Comparator<Job> comparator = new JobAscByPriority();
         int rsl = comparator.compare(job1, job2);
         assertThat(rsl, is(-1));
     }
 
     @Test
     void whenDescByName() {
-        comparator = new JobDescByName();
+        Job job1 = new Job("Fix bug", 0);
+        Job job2 = new Job("Fix bug", 4);
+        Comparator<Job> comparator = new JobDescByName();
         int rsl = comparator.compare(job1, job2);
         assertThat(rsl, is(0));
     }
 
     @Test
     void whenDescByPriority() {
-        comparator = new JobDescByPriority();
+        Job job1 = new Job("Fix bug", 0);
+        Job job2 = new Job("Fix bug", 4);
+        Comparator<Job> comparator = new JobDescByPriority();
         int rsl = comparator.compare(job1, job2);
         assertThat(rsl, is(1));
     }
 
     @Test
     void byDescNameAndDescPriority() {
-        comparator = new JobDescByName().thenComparing(new JobDescByPriority());
+        Job job1 = new Job("Fix bug", 0);
+        Job job2 = new Job("Fix bug", 4);
+        Comparator<Job> comparator = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = comparator.compare(job1, job2);
         assertThat(rsl, greaterThan(0));
     }
 
     @Test
     void byAscNameAndAscPriority() {
-        comparator = new JobAscByName().thenComparing(new JobAscByPriority());
+        Job job1 = new Job("Fix bug", 0);
+        Job job2 = new Job("Fix bug", 4);
+        Comparator<Job> comparator = new JobAscByName().thenComparing(new JobAscByPriority());
         int rsl = comparator.compare(job1, job2);
         assertThat(rsl, lessThan(0));
     }

fa4ee290fa5569da08025deffc37601cf01b5003
Arthur Magomedov <devmagomedov@gmail.com>
2022-09-09 19:46:49 +0300

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] str1 = o1.split("/");
+        String[] str2 = o2.split("/");
+        int rsl = str2[0].compareTo(str1[0]);
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
+    }
+}

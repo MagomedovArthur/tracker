24be1eef671ee613eeeabdecc9df4770227e6bdb
Arthur Magomedov <devmagomedov@gmail.com>
2022-09-10 13:23:35 +0300

    2. Отсортировать департаменты [#34352 #341568]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -9,10 +9,7 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                if (el.startsWith("s")) {
-                    start += "/";
-                }
-                start = start + el;
+                start += "".equals(start) ? el : "/" + el;
                 tmp.add(start);
             }
         }

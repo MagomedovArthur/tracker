493672d59e4ef582c6a13ef34906d2f87db695ac
Arthur Magomedov <devmagomedov@gmail.com>
2023-08-13 12:58:24 +0300

    1. Что такое Mock-объект. [#504866 #436422]

--- src/test/java/ru/job4j/tracker/FindItemByIdActionTest.java
+++ src/test/java/ru/job4j/tracker/FindItemByIdActionTest.java
@@ -11,14 +11,12 @@ import static org.mockito.Mockito.when;
 
 class FindItemByIdActionTest {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-
     @Test
     public void execute() {
         Output out = new StubOutput();
         MemTracker tracker = new MemTracker();
         tracker.add(new Item("Item1"));
-        tracker.add(new Item("Item2"));
+        var result = tracker.add(new Item("Item2"));
         FindItemByIdAction findItemByIdAction = new FindItemByIdAction(out);
 
         Input input = mock(Input.class);
@@ -28,8 +26,6 @@ class FindItemByIdActionTest {
         findItemByIdAction.execute(input, tracker);
 
         String ln = System.lineSeparator();
-        String result = String.format("Item{id=%s, name='%s', created=%s}",
-                tracker.findById(2).getId(), tracker.findById(2).getName(), tracker.findById(2).getCreated().format(FORMATTER));
         assertThat(out.toString()).isEqualTo("=== Find item by id ===" + ln + result + ln);
         assertThat(tracker.findById(2).getName()).isEqualTo("Item2");
     }
--- src/test/java/ru/job4j/tracker/FindItemByNameActionTest.java
+++ src/test/java/ru/job4j/tracker/FindItemByNameActionTest.java
@@ -11,25 +11,21 @@ import static org.mockito.Mockito.when;
 
 class FindItemByNameActionTest {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-
     @Test
     public void execute() {
         Output out = new StubOutput();
         MemTracker tracker = new MemTracker();
-        tracker.add(new Item("Item1"));
+        var result = tracker.add(new Item("Item1"));
         tracker.add(new Item("Item2"));
-        FindItemByNameAction findItemByIdAction = new FindItemByNameAction(out);
+        FindItemByNameAction findItemByNameAction = new FindItemByNameAction(out);
 
         Input input = mock(Input.class);
 
         when(input.askStr(any(String.class))).thenReturn("Item1");
 
-        findItemByIdAction.execute(input, tracker);
+        findItemByNameAction.execute(input, tracker);
 
         String ln = System.lineSeparator();
-        String result = String.format("Item{id=%s, name='%s', created=%s}",
-                tracker.findById(1).getId(), tracker.findById(1).getName(), tracker.findById(1).getCreated().format(FORMATTER));
         assertThat(out.toString()).isEqualTo("=== Find items by name ===" + ln + result + ln);
         assertThat(tracker.findById(1).getName()).isEqualTo("Item1");
     }
